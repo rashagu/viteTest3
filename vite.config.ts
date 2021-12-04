95af10ed7ff8dce62b2f45e500151473753097b4
kousum <1135192606@qq.com>
2021-12-04 14:50:00 +0800

    test2

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue()
+    vue(),
+    legacy({
+      targets: ['Chrome 63'],
+      modernPolyfills: true
+    })
   ]
 })
